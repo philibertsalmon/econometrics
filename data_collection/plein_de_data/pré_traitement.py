d57bbffc0bbc57677689c533e4ee765ce40bc760
philibertsalmon <philibert.salmon@mines-paristech.fr>
2022-09-21 15:15:01 +0200

    data .txt nettoyées -> csv

--- data_collection/plein_de_data/pré_traitement.py
+++ data_collection/plein_de_data/pré_traitement.py
@@ -1,3 +1,27 @@
 import pandas as pd
 
-df = pd.read_csv('walmart_opening_1962_2006.csv')
+# Pour wallmart_exit2016.txt :
+"""
+with open('wallmart_exit2016.txt') as f:
+    content = f.read()
+
+content = content.replace('    \n', '\n')
+content = content.replace('   \n', '\n')
+content = content.replace('#', '')
+content = content.replace(':', ',')
+content = content.replace('    ', ', ')
+categories = content.split('!')
+
+data =[]
+
+for category in categories:
+    lines = category.split('\n')
+    category_name = lines[0]
+    for line in lines[1:]:
+        splitted_line = line.split(', ')
+        if line!='':
+            data.append(splitted_line)
+
+df = pd.DataFrame(data, columns=['', 'Street', 'Town', 'State', 'Date of exit'])
+
+df.to_csv('wallmart_exit2016_données_traitées.csv')"""
